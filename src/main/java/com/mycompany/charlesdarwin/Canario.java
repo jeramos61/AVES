1141a0f3daaa18b2862479290b305e258c8f0642
johan <johan@LAPTOP-34H9993E>
2024-02-10 18:25:26 -0500

    clases de aves

---
+++ src/main/java/com/mycompany/charlesdarwin/Canario.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.charlesdarwin;
+
+/**
+ *
+ * @author johan
+ */
+public class Canario extends Ave {
+
+    @Override
+    public void makesound() {
+        
+        System.out.println("Canta");
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
